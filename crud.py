a453e06e26a2ed1949ef1edf674687bb0cf14dc1
basita512 <mail2basita@gmail.com>
2026-02-18 19:55:39 +0530

    CrudOperation Done

---
+++ crud.py
@@ -0,0 +1,89 @@
+from fastapi import FastAPI, status
+from pydantic import BaseModel
+from fastapi.exceptions import HTTPException
+
+
+books = [
+    {
+        "id" : 1,
+        "title": "The alchemist",
+        "author": "Paulo Coelho",
+        "publish_date": "1988-01-01"
+    },
+
+    {
+        "id" : 2,
+        "title": "The God of small Things",
+        "author": "Arundhati Roy",
+        "publish_date": "1997-04-04"
+    },
+
+    {
+        "id" : 3,
+        "title": "The White Tiger",
+        "author": "Aravind Adiga",
+        "publish_date": "2008-01-01"
+    },
+
+    {
+        "id" : 4,
+        "title": "The Palace of illusions",
+        "author": "Chitra Banerjee divakaruni",
+        "publish_date": "2008-02-12"
+    },
+]
+
+app = FastAPI()
+
+@app.get("/book")
+def get_book():
+    return books 
+
+@app.get("/book/{book_id}")
+def get_book(book_id: int):
+    for book in books:
+        if book['id'] == book_id:
+            return book
+        
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = "Book not found" )
+
+class Book(BaseModel):
+    id: int
+    title: str
+    author: str
+    publish_date:str
+
+@app.post("/book")
+def create_book(book: Book):
+    new_book = book.model_dump()
+    books.append(new_book)
+    return new_book
+
+#pyrentic class
+class Bookupdate(BaseModel):
+        title: str
+        author: str
+        publish_date: str
+
+@app.put("/book/{book_id}")
+def update_book(book_id: int, book_update: Bookupdate):
+     for book in books:
+        if book['id'] == book_id:
+            book['title'] = book_update.title
+            book['author'] = book_update.author
+            book['publish_date'] = book_update.publish_date
+            return book
+         
+     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = "Book not found")
+
+
+
+@app.delete("/book/{book_id}")
+def delete_book(book_id:int):
+    for book in books:
+        if book['id'] == book_id:
+            books.remove(book)
+            return {"Message": "Our Book Deleted"}
+        
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail = "Book not found")  
+        
